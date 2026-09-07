b8aeb046d5df9b542208b72de907a8cd0ec6f202
Juniza Permata Nugraheni <junizanugraheni@gmail.com>
2026-09-07 10:39:13 +0700

    fixing

--- src/component/ui/Text/text.types.ts
+++ src/component/ui/Text/text.types.ts
@@ -1,7 +1,7 @@
 import { VariantProps } from "class-variance-authority";
 import { HTMLAttributes } from "react";
-import { textVariants } from "./Text.styles";
+import { TextVariants } from "./Text.styles";
 
 export interface TextProps
     extends HTMLAttributes<HTMLParagraphElement>,
-    VariantProps<typeof textVariants> { }
+    VariantProps<typeof TextVariants> { }
